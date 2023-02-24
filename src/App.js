5e8a152122b8f293af6798f1cfadd12b65df75cf
taehyeon0412 <71374539+taehyeon0412@users.noreply.github.com>
2023-02-25 02:07:27 +0900

    style component study (3)
    as를 이용하여 원하는 태그로 바꿀 수 있다.
    ex) <Btn as="a">Log in</Btn> 으로 적으면 "button"대신
    as에 적힌 "a"가 태그로 된다

--- src/App.js
+++ src/App.js
@@ -18,11 +18,28 @@ const Circle = styled(Box)`
 /* style 컴포넌트를 쓸 때에는 백틱 ``을 사용한다 */
 /* props를 이용하여 코드를 간소화 할 수 있다 */
 
+const Btn = styled.button`
+  color: white;
+  background-color: tomato;
+  border: 0;
+  border-radius: 15px;
+`;
+
+/* styled.[태그]에 html태그를 쓰고 싶지않을때에는
+as를 사용해서 바꿔줄 수 있다.
+ex) <Btn as="a">Log in</Btn> 으로 적으면 "button"대신 
+as에 적힌 "a"가 태그로 된다
+*/
+
 function App() {
   return (
     <Father>
       <Box bgColor="teal" />
       <Circle bgColor="green" />
+      <Btn>Log in</Btn>
+      <Btn as="a" href="/">
+        Log in
+      </Btn>
     </Father>
   );
 }
