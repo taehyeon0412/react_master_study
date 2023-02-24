6a58619d06cb17c0ac5cfbb622d5b4d2884bbe4a
taehyeon0412 <71374539+taehyeon0412@users.noreply.github.com>
2023-02-25 03:02:25 +0900

    style component study (5)
    스타일컴포넌트에서 애니메이션을 쓰려면 import에 {keyframes}를 추가한다

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
-const Father = styled.div`
+const Wrapper = styled.div`
   display: flex;
 `;
 
-const Input = styled.input.attrs({ required: true })`
-  background-color: teal;
+const boxAni = keyframes`
+  0% {
+    transform:rotate(0deg); //deg = degree(각도)
+    border-radius:0px;
+  }
+  50% {
+    border-radius:100px;
+  }
+  100% {
+    transform:rotate(360deg);
+    border-radius:0px;
+  }
+
+`;
+
+const Box = styled.div`
+  height: 200px;
+  width: 200px;
+  background-color: green;
+  animation: ${boxAni} 2s linear infinite;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  span {
+    font-size: 30px;
+    &:hover {
+      font-size: 60px;
+    }
+  } //Box안에 있는 개체를 컴포넌트 스타일 안에 쓸 수 있다.
+  //&는 바로위의 태그를 뜻함 span:hover 대신 &:hover로 짧게 쓸 수 있다.
 `;
 
 function App() {
   return (
-    <Father>
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-    </Father>
+    <Wrapper>
+      <Box>
+        <span>😂</span>
+      </Box>
+    </Wrapper>
   );
 }
 
@@ -30,3 +57,7 @@ as에 적힌 "a"가 태그로 된다
 /*공통된속성을 attrs를 이용하여 한번에 부여할 수 있다. 
 attrs = Attributes(속성) 줄임말
 */
+
+/* 스타일컴포넌트에서 애니메이션을 쓰려면 import에 {keyframes}를 추가한다
+
+ */
