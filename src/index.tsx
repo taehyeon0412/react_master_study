fa4018f4b0811a9e81795ca574967ba93d099e07
taehyeon0412 <71374539+taehyeon0412@users.noreply.github.com>
2023-03-01 22:46:31 +0900

    react-query install

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "styled-components";
 import App from "./App";
-import { darkTheme, lightTheme } from "./theme";
+import { Theme } from "./theme";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={Theme}>
       <App />
     </ThemeProvider>
   </React.StrictMode>
