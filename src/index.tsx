8dbdf46f94c04578a92bfc204d35f26ae41fe256
taehyeon0412 <71374539+taehyeon0412@users.noreply.github.com>
2023-03-01 17:46:36 +0900

    typescript props (4)
    styled.d.ts 파일을 생성해서 테마 적용을 함

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "styled-components";
 import App from "./App";
+import { darkTheme, lightTheme } from "./theme";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
 /* Theme를 사용하려면 index에 ThemeProvider를 import하고 
-ThemeProvider를 사용할 곳에 감싸주고 theme라는 prop를 사용해준다
+ThemeProvider를 사용할 곳에 감싸주고 theme라는 proimport { lightTheme } from './theme';
+p를 사용해준다
 */
