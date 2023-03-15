1c3103dd5f221bb73d1221cf65f09e5757ac30c6
taehyeon0412 <71374539+taehyeon0412@users.noreply.github.com>
2023-03-15 18:36:31 +0900

    ToDoList(3)
    Click event로 카테고리 클릭시 인자를 넘겨받을 수 있게 함

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,14 +1,51 @@
-import { IToDo } from "../atom";
+import { useSetRecoilState } from "recoil";
+import { IToDo, toDoState } from "../atom";
 
-function ToDo({ text }: IToDo) {
+function ToDo({ text, category, id }: IToDo) {
+  const setToDos = useSetRecoilState(toDoState);
+
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name },
+    } = event;
+  };
+
   return (
     <li>
       <span>{text}</span>
-      <button>To Do</button>
-      <button>Doing</button>
-      <button>Done</button>
+
+      {category !== "TO_DO" && (
+        <button name="TO_DO" onClick={onClick}>
+          To Do
+        </button>
+      )}
+      {category !== "DOING" && (
+        <button name="DOING" onClick={onClick}>
+          Doing
+        </button>
+      )}
+      {category !== "DONE" && (
+        <button name="DONE" onClick={onClick}>
+          Done
+        </button>
+      )}
     </li>
   );
 }
 
 export default ToDo;
+
+/* 인자가 있는 event를 처리하는 법
+기본 사용은 onClick={onClick}인데
+인자가 있는 event를 처리할때에는 onClick ={()=>onClick"인자"}로 한다.
+*/
+
+/* newCategory: "TO_DO" | "DOING" | "DONE"; 을
+   newCategory: IToDo["category"]로 짧게 쓸 수 있다.*/
+
+/* 1.인자를 받는 함수를 직접 만들고 버튼onClick 이벤트에 인자를 넘겨주고
+클릭시 인자를 알 수 있는 방법
+
+   2.인자로 받지 않는 대신 각각의 버튼에 각각 name을 지정
+    click event가 발생하면 event.currentTarget.name으로 알 수 있는 방법
+ */
